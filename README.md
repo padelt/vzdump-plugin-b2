61c22c1d09a046e0b2140835042d4581d70d2b0b
Philipp Adelt <autosort-github@philipp.adelt.net>
2016-02-11 11:34:41 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# vzdump-plugin-b2
-Encrypted off-site backup of Proxmox VE vzdump images for $5/month/TB
+# Encrypted off-site backup of Proxmox VE vzdump images for $5/month/TB
+
+This plugin to `vzdump` will hook into after a single VM backup is done.
+It will 
+- split the `.vma.lzo` file that `vzdump` wrote to chunks of configurable size
+  (default is 2 GB, B2 allows up to 5 GB=5*10^9 byte)
+- encrypt them using a symmetric key (password - no GPG keys used) and 
+- upload them to Backblaze B2 to a configurable path (default: hostname)
+ under a configurable bucket in parallel.
+
+Every step is checksummed and the checksum file is uploaded as well.
+
+After all chunk uploads are finished, **the local copy of the backup is deleted**.
+Therefore you only need twice the size of the largest VM's backup file
+on the host. The backup is read and written multiple times, so using different
+storage devices for VMs and backup is a good idea.
+
+## Preparation
+
+1. Get a Backblaze account and generate an application key along with a bucket.
+1. Install git and download this repository:
+   ```
+   apt-get install -y git
+   git clone https://github.com/padelt/vzdump-plugin-b2.git /usr/local/bin/vzdump-plugin-b2
+   ```
+    Alternatively transfer it manually to the server.
+1. Make a copy of `upload-b2.config.template` and edit it to your parameters.
+   If you put it anywhere else than the filename `upload-b2.config` in the
+   same directory as `vzdump-plugin-upload-b2.sh`, also edit that script and
+   make `CONFIG_FILE` point to it.
+1. Make `vzdump` aware of the script by adding a line to `/etc/vzdump.conf`:
+   ```
+   echo "script: /usr/local/bin/vzdump-plugin-b2/vzdump-script-upload-b2.sh" >> /etc/vzdump.conf
+   ```
+1. Make available the `b2` command-line utility as documented [here](https://www.backblaze.com/b2/docs/quick_command_line.html):
+   ```
+   curl -o /usr/local/bin/b2 https://docs.backblaze.com/public/b2_src_code_bundles/b2
+   chmod +x /usr/local/bin/b2
+   ```
+
+## Testing
+
+A manual backup of a small VM is a good idea to test general functionality.
+Look at the log output for hints what may have gone wrong.
---
+++ vzdump-plugin-upload-b2.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+CONFIG_FILE=$(dirname $0)/upload-b2.config
+
+. "$CONFIG_FILE"
+
+if [ -r "$CONFIG_FILE" ] ; then
+  echo "Where is my config file? Looked in $CONFIG_FILE."
+  echo "If you have none, copy the template and enter your information."
+  echo "If it is somewhere else, change the second line of this script."
+  exit 1
+fi
+if [ ! -x "$GPG_BINARY" ] || [ ! -x "$B2_BINARY" ] || [ -r "$GPG_PASSPHRASE_FILE" ] ; then
+  echo "Missing one of $GPG_BINARY, $B2_BINARY or $GPG_PASSPHRASE_FILE."
+  echo "Or one of the binaries is not executable."
+  echo 2
+fi
+
+# Eliminate duplicate slashes. B2 does not accept those in file paths.
+TARFILE=$(sed 's#//#/#g' <<< "$TARFILE")
+TARBASENAME=$(basename "$TARFILE")
+
+#echo "PHASE: $1"
+#echo "MODE: $2"
+#echo "VMID: $3"
+#echo "VMTYPE: $VMTYPE"
+#echo "DUMPDIR: $DUMPDIR"
+#echo "HOSTNAME: $HOSTNAME"
+#echo "TARFILE: $TARFILE"
+#echo "LOGFILE: $LOGFILE"
+#echo "USER: `whoami`"
+
+if [ "$1" == "backup-end" ]; then
+  if [ ! -f $TARFILE ] ; then
+    echo "Where is my tarfile?"
+    exit 3
+  fi
+
+  echo "CHECKSUMMING whole tar."
+  sha1sum "$TARFILE" >> "$TARFILE.sha1sums"
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong checksumming."
+    exit 4
+  fi
+
+  echo "SPLITTING into chunks sized <=$B2_SPLITSIZE_BYTE byte"
+  time split --bytes=$B2_SPLITSIZE_BYTE --suffix-length=3 --numeric-suffixes "$TARFILE" "$TARFILE.split."
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong splitting."
+    exit 5
+  fi
+
+  echo "CHECKSUMMING splits"
+  sha1sum $TARFILE.split.* >> "$TARFILE.sha1sums"
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong checksumming."
+    exit 6
+  fi
+
+  echo "Deleting whole file"
+  rm "$TARFILE"
+
+  echo "ENCRYPTING"
+  ls -1 $TARFILE.split.* | time xargs -I % -n 1 -P $NUM_PARALLEL_GPG $GPG_BINARY --no-tty --compress-level 0 --passphrase-file $GPG_PASSPHRASE_FILE -c --output "%.gpg" "%"
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong encrypting."
+    exit 7
+  fi
+
+  echo "Checksumming encrypted splits"
+  sha1sum $TARFILE.split.*.gpg >> "$TARFILE.sha1sums"
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong checksumming."
+    exit 8
+  fi
+
+  echo "Deleting cleartext splits"
+  rm $TARFILE.split.???
+
+  echo "AUTHORIZING AGAINST B2"
+  $B2_BINARY authorize_account $B2_ACCOUNT_ID $B2_APPLICATION_KEY
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong authorizing."
+    exit 9
+  fi
+
+  echo "UPLOADING to B2 with up to $NUM_PARALLEL_UPLOADS parallel uploads."
+  ls -1 $TARFILE.sha1sums $TARFILE.split.* | xargs -I % -n 1 -P $NUM_PARALLEL_UPLOADS $B2_BINARY upload_file $B2_BUCKET "%" "$B2_PATH%"
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong uploading."
+    exit 10
+  fi
+
+  echo "REMOVING older remote backups."
+  $B2_BINARY list_file_names $B2_BUCKET | /root/bin/jq --arg vmid $3 --arg fn "$TARBASENAME" '.files[]|select(.fileName|test(".*vzdump-qemu-"+$vmid+".*"))|select((.fileName|test(".*"+$fn+".*")==false))|"/root/bin/b2 delete_file_version "+.fileName+" "+.fileId' | xargs -n 1 -r -I % bash -c "%"
+  if [ $? -ne 0 ] ; then
+    echo "Something went wrong deleting old versions."
+    exit 11
+  fi
+
+  echo "DELETING local encrypted splits"
+  rm $TARFILE.split.*.gpg
+    
+fi
